e3dce05979c0c2119a85c48b5c4011e846f09db5
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2020-01-17 20:04:42 +0600

    ADD: CustomMap class to wrap google api.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
 console.log(user);
 
 const company = new Company();
 console.log(company);
+
+const customMap = new CustomMap("map");
